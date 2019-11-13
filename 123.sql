cc477219c89075a0fc4a965baa5ebdaf9d26959c
santoshaland <alandsantosh2@gmail.com>
2019-11-13 15:30:43 +0530

    sql commited

---
+++ 123.sql
@@ -0,0 +1,11 @@
+drop procedure if exists sp_param;
+delimiter $$
+Create procedure sp_param(IN a int ,IN b int ,OUT c int ,INOUT d int)
+BEGIN
+	SET c = a + b;
+	SET d = d * d;
+END;
+$$
+DELIMITER ;
+
+
